12405727b4187e05788a79b1406b3d60728260e0
diego <diegoespil92@gmail.com>
2019-09-18 17:56:50 -0300

    agregando atributos a entidad

--- Proyecto/src/Entidad/Entidad.java
+++ Proyecto/src/Entidad/Entidad.java
@@ -1,8 +1,24 @@
 package Entidad;
 
+import java.awt.Point;
+
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
 public abstract class Entidad {
 	
-	String nombre;
-	int vida;
+	protected String nombre;
+	protected int vida;
+	protected JLabel grafico;
+	protected Icon image [];
+	protected int velocidad;
+	protected Point pos;
+	
+	
+	public Entidad(int x, int y, int velocidad) {
+		this.pos = new Point(x,y);
+		this.velocidad = velocidad;
+		image = new Icon[4];
+	}
 	
 }
